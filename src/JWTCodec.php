0c3d0614a722a8d33b8cef46d0df7e978e12565b
SkipEveryLunch <harima.yasuhiro@outlook.jp>
2022-04-20 23:48:51 +0900

    fixed a bug in encode method

--- src/JWTCodec.php
+++ src/JWTCodec.php
@@ -6,10 +6,12 @@ class JWTCodec{
       "alg" => "HS256"
     ]);
     $header = $this->base64UrlEncode($header);
-    $payload = $this->base64UrlEncode(json_encode($payload));
+    $payload = json_encode($payload);
+    $payload = $this->base64UrlEncode($payload);
     $signature = hash_hmac("sha256",
     $header . "." . $payload,
-    "5A7134743777217A25432A462D4A614E645266556A586E3272357538782F413F");
+    "5A7134743777217A25432A462D4A614E645266556A586E3272357538782F413F",true);
+    $signature = $this->base64urlEncode($signature);
     return $header . "." . $payload . "." . $signature;
   }
   public function decode(string $token){
@@ -20,7 +22,6 @@ class JWTCodec{
     $matches["header"] . "." . $matches["payload"],
     "5A7134743777217A25432A462D4A614E645266556A586E3272357538782F413F",true);
     $signature_from_token = $this->base64UrlDecode($matches["signature"]);
-    echo $signature . " : " .$signature_from_token;
     if(!hash_equals($signature,$signature_from_token)){
       throw new Exception("signature doesn't match");
     };
