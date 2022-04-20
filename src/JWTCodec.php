e34b52df9bade9ad98c31a41949e92841f970427
SkipEveryLunch <harima.yasuhiro@outlook.jp>
2022-04-20 23:35:53 +0900

    impelemented decode system

--- src/JWTCodec.php
+++ src/JWTCodec.php
@@ -12,12 +12,34 @@ class JWTCodec{
     "5A7134743777217A25432A462D4A614E645266556A586E3272357538782F413F");
     return $header . "." . $payload . "." . $signature;
   }
+  public function decode(string $token){
+    if(preg_match("/^(?<header>.+)\.(?<payload>.+)\.(?<signature>.+)$/",$token,$matches)!==1){
+      throw new InvalidArgumentException("invalid token format");
+    }
+    $signature = hash_hmac("sha256",
+    $matches["header"] . "." . $matches["payload"],
+    "5A7134743777217A25432A462D4A614E645266556A586E3272357538782F413F",true);
+    $signature_from_token = $this->base64UrlDecode($matches["signature"]);
+    echo $signature . " : " .$signature_from_token;
+    if(!hash_equals($signature,$signature_from_token)){
+      throw new Exception("signature doesn't match");
+    };
+    $payload = json_decode($this->base64UrlDecode($matches["payload"]),true);
+    return $payload;
+  }
   private function base64UrlEncode(string $text):string{
     return str_replace(
       ["+","/","="],
       ["-","_",""],
       base64_encode($text)
     );
+  }
+  private function base64UrlDecode(string $text):string{
+    return base64_decode(str_replace(
+      ["-","_"],
+      ["+","/"],
+      $text
+    ));
   }
 }
 ?>
