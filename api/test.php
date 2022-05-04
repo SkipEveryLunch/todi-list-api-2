1da3d23aa8e9882f96fe5b51626f023773f7775f
SkipEveryLunch <harima.yasuhiro@outlook.jp>
2022-05-04 13:46:33 +0900

    for testing

--- api/test.php
+++ api/test.php
@@ -4,7 +4,6 @@
 
   header('Access-Control-Allow-Origin: *');
   header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  header('Access-Control-Allow-Headers *');
-  
+
   echo json_encode("this is a test");
 ?>
