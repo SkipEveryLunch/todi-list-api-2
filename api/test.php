db89d23a4694d911e3347b3a53b8ba4daa035b79
SkipEveryLunch <harima.yasuhiro@outlook.jp>
2022-05-04 13:45:45 +0900

    for testing

--- api/test.php
+++ api/test.php
@@ -1,5 +1,10 @@
 <?php
   declare(strict_types=1);
   require __DIR__ . '/bootstrap.php';
+
+  header('Access-Control-Allow-Origin: *');
+  header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  header('Access-Control-Allow-Headers *');
+  
   echo json_encode("this is a test");
 ?>
