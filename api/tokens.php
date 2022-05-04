50b332b53118a1825f7de083b3b88ea5a120a8b5
SkipEveryLunch <harima.yasuhiro@outlook.jp>
2022-05-04 12:42:03 +0900

    for testing

--- api/tokens.php
+++ api/tokens.php
@@ -10,8 +10,9 @@ $refresh_token = $codec->encode([
   'sub'=>$user['id'],
   'exp'=>$refresh_token_expiry
 ]);
-echo json_encode([
-  'access_token'=>$access_token,
-  'refresh_token'=>$refresh_token
-]);
+// echo json_encode([
+//   'access_token'=>$access_token,
+//   'refresh_token'=>$refresh_token
+// ]);
+echo json_encode("hello");
 ?>
