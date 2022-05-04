d09cb01e43a42f174f00387e4226e97d41dedf97
SkipEveryLunch <harima.yasuhiro@outlook.jp>
2022-05-04 13:55:41 +0900

    get rid of allow-access-header:* from register.php

--- api/register.php
+++ api/register.php
@@ -6,7 +6,6 @@
 
   header("Access-Control-Allow-Origin: *");
   header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, PATCH, DELETE");
-  header("Access-Control-Allow-Headers *");
 
   if($_SERVER["REQUEST_METHOD"] !== "POST"){
     http_response_code(405);
